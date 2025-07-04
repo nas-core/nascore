dee80116d6cd673096665915d9dfac774fae5f8f
joyanhui <leiyanhui@gmail.com>
2025-07-05 00:11:55 +0800

    反向代理功能适配

--- nascore_handler_http/sub_reverseproxy/SubNasCoreVodSoket.go
+++ nascore_handler_http/sub_reverseproxy/SubNasCoreVodSoket.go
@@ -1,51 +1,44 @@
 package sub_reverseproxy
 
 import (
-	"context" // Needed for DialContext
-	"net"     // Needed for DialContext
+	"net"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"strings"
-	"time" // For IdleConnTimeout
+	"time"
 
-	"github.com/nas-core/nascore/nascore_handler_http/index_and_favicon"
 	"github.com/nas-core/nascore/nascore_util/system_config"
-
 	"go.uber.org/zap"
 )
 
 func SubNasCoreVodSoket(subPathPrefix string, unixSocketPath *string, cfg *system_config.SysCfg, logger *zap.SugaredLogger, qpsCounter *uint64) http.HandlerFunc {
-	// 创建一个自定义的 HTTP 传输层，用于连接到 Unix Socket
-	unixSocketTransport := &http.Transport{
-		DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
-			return net.Dial("unix", *unixSocketPath)
-		},
-
-		DisableKeepAlives: true,
-		IdleConnTimeout:   30 * time.Second,
-	}
-
-	proxy := &httputil.ReverseProxy{
-		Director: func(req *http.Request) {
-			originalPath := req.URL.Path                                  // 获取原始请求路径
-			targetPath := strings.TrimPrefix(originalPath, subPathPrefix) // 移除前缀，得到后端服务的实际路径
-			req.URL.Scheme = "http"
-			req.URL.Host = "unixsocket"
-			req.URL.Path = "/" + targetPath // 后端服务的实际请求路径e
-			req.Host = req.URL.Host
-			req.Header.Set("Connection", "close")
-		},
-		Transport: unixSocketTransport,
-		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
-			logger.Errorf("NasCoreVod proxy backend error: %v", err)
-			englishMsg := "NasCoreVod might not be running. Please start it or enable auto-start. If already running, ensure Nascore server/container/VM can access the backend URL."
-			chineseMsg := "NasCoreVod 可能未启动。请手动启动或开启随启动。如果已启动，请确保 Nascore 服务器/容器/虚拟机可访问后端地址。"
-			// 在错误页面中包含后端 Unix Socket 路径，帮助诊断问题
-			// Include the backend Unix Socket path in the error page to help diagnose the issue
-			index_and_favicon.RenderPage(w, "NasCoreVod Backend Error 502", englishMsg+"     --------------------ErrInfo--------------------  "+*unixSocketPath+" ------------ "+err.Error(), chineseMsg)
-		},
-	}
 	return func(w http.ResponseWriter, r *http.Request) {
+		target, err := url.Parse("http://unix")
+		if err != nil {
+			logger.Errorw("failed to parse target URL", "error", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		transport := &http.Transport{
+			Dial: func(_, _ string) (net.Conn, error) {
+				return net.Dial("unix", *unixSocketPath)
+			},
+			ResponseHeaderTimeout: 60 * time.Second, // 设置等待后端响应头的超时时间
+			IdleConnTimeout:       90 * time.Second, // 设置连接保持空闲的最长时间
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(target)
+		proxy.Transport = transport
+
+		// 如果 r.URL.Path 里面不包含 字符串 proxy/
+		if !strings.Contains(r.URL.Path, "proxy/") {
+			r.URL.Path = strings.TrimPrefix(r.URL.Path, subPathPrefix)
+			if r.URL.Path == "" {
+				r.URL.Path = "/"
+			}
+		}
 		proxy.ServeHTTP(w, r)
 	}
 }
